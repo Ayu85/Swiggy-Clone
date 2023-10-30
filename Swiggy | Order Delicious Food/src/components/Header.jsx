3d0e9558f0e5124fdf3f77014bfb5fb90b8fb53e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-30 14:05:13 +0000

    header part done

--- Swiggy | Order Delicious Food/src/components/Header.jsx
+++ Swiggy | Order Delicious Food/src/components/Header.jsx
@@ -1,9 +1,19 @@
-import logo from '../assets/pngwing.com'
-function Header(){
-    return(
+import logo from '../assets/pngwing.com.png'
+function Header() {
+    return (
         <div className="header-bar">
             <div className="logo">
-                <img src={logo}/>
+                <img src={logo} width={130} />
+            </div>
+            <div className='nav-buttons'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className='log-btn'>
+                <button>Login</button>
             </div>
         </div>
     )
