b9fa9552d134ad8fd478c95fa2e39c6b59dd2d8b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-01 19:39:10 +0000

    data rendering done

--- Swiggy | Order Delicious Food/src/components/Body.jsx
+++ Swiggy | Order Delicious Food/src/components/Body.jsx
@@ -1,18 +1,45 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
 import { restaurantData } from "../utils/mockData";
+import axios from 'axios';
 function Body() {
     const [searchText, setsearchText] = useState("");
     const [originalRestaurantData, setoriginalRestaurantData] = useState([]);
+    const [filteredRestaurantData, setfilteredRestaurantData] = useState([]);
+    const [restaurantAPIdata, setrestaurantAPIdata] = useState([]);
+    // useEffect(() => {
+    //     getRestaurantData();
+    //     getRestaurantDataAPI();
+    // }, [])
+    // const getRestaurantDataAPI = async () => {
+    //     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.321684&lng=82.987289&page_type=DESKTOP_WEB_LISTING")
+    //     const json = await data.json();
+    //     console.log(json);
+    //     const usableData = await json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    //     setoriginalRestaurantData(usableData);
+    //     setfilteredRestaurantData(usableData);
+
+    // }
     useEffect(() => {
-        getRestaurantDataAPI();
+        getd();
     }, [])
-    function getRestaurantDataAPI() {
-        setoriginalRestaurantData(restaurantData);
-        console.log(restaurantData);
+    const getd = async () => {
+        const d1 = await axios.get("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.321684&lng=82.987289&page_type=DESKTOP_WEB_LISTING")
+        setfilteredRestaurantData(d1?.data?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setoriginalRestaurantData(d1?.data?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        console.log(d1);
     }
-
-
+    // function getRestaurantData() {
+    //     setfilteredRestaurantData(restaurantData);
+    //     console.log(restaurantData);
+    // }
+    function getFilteredData() {
+        return originalRestaurantData.filter((items) => {
+            return items?.info?.name?.toLowerCase()?.includes(searchText?.toLowerCase());
+        })
+    }
+    // if (filteredRestaurantData.length === 0)
+    //     return <h1 className="filter-error">sorry....no data found </h1>
     return (
 
         <div>
@@ -20,13 +47,15 @@ function Body() {
                 <input type="text" placeholder="Find taste near you" value={searchText} onChange={(e) => {
                     setsearchText(e.target.value)
                 }} />
-                <button>
+                <button onClick={() => {
+                    setfilteredRestaurantData(getFilteredData(searchText));
+                }}>
                     Find
                 </button>
             </div>
 
             <div className="body">
-                {originalRestaurantData.map((items) => {
+                {filteredRestaurantData?.map((items) => {
                     return <Card {...items?.info} />
                 })}
             </div>
--- Swiggy | Order Delicious Food/src/components/Card.jsx
+++ Swiggy | Order Delicious Food/src/components/Card.jsx
@@ -1,11 +1,16 @@
 import { IMG_URL } from "../utils/Config";
-function Card({name,cloudinaryImageId,cuisines,areaName}){
-    return(
+function Card({ name, cloudinaryImageId, cuisines, areaName, avgRatingString, sla }) {
+    return (
         <div className="card">
-            <img src={IMG_URL+cloudinaryImageId}/>
-            <h2>{name}</h2>
-            <h3>{cuisines?.join(',')}</h3>
-            <h3>{areaName}</h3>
+            <img src={IMG_URL + cloudinaryImageId} />
+            <div className="card-attributes">
+                <h2>{name}</h2>
+                <div className="rating-time">
+                    <h4> ⭐{avgRatingString}</h4>
+                    <h4>📌  {sla?.deliveryTime}  mins</h4></div>
+                <h4 className="light-font cuisines">{cuisines?.slice(0, 4)?.join(",")}</h4>
+                <h4 className="light-font " >🌏{areaName}</h4>
+            </div>
         </div>
     )
 }
