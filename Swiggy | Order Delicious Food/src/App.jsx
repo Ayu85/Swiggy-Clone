f2c3ce713fb04f7bd400fbe1c38aa0d658e92088
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-03 06:50:04 +0000

    api fetching done

--- Swiggy | Order Delicious Food/src/App.jsx
+++ Swiggy | Order Delicious Food/src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
 import Body from './components/Body'
 import Header from './components/Header'
+import RestaurantMenu from './components/RestaurantMenu'
+import { BrowserRouter, Routes, Route, createBrowserRouter, Outlet } from "react-router-dom"
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Body />
+      },
+      {
+        path: '/restaurant/:id',
+        element: <RestaurantMenu />
+      }
+    ]
+  }
+])
 function App() {
 
   return (
     <>
       <Header />
-      <Body/>
+      <Outlet/>
+
+
     </>
   )
 }
 
-export default App
+export default appRouter;
