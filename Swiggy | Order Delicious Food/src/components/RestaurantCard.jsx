6a7d739ced655978349d87f5b646e565cd9a2ada
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-04 14:11:16 +0000

    final styling done

--- Swiggy | Order Delicious Food/src/components/RestaurantCard.jsx
+++ Swiggy | Order Delicious Food/src/components/RestaurantCard.jsx
@@ -3,8 +3,8 @@ function RestaurantCard({ imageId, name, price, defaultPrice,description }) {
     return (
         <div className="menu-card">
             <div>
-                <h2>{name}</h2>
-                <h3>&#8377;{price / 100 || defaultPrice / 100}</h3>
+                <h4 className="restaurant-card-name">{name}</h4>
+                <h4>&#8377;{price / 100 || defaultPrice / 100}</h4>
                 <p>{description}</p>
             </div>
             <img src={IMG_URL + imageId} />
