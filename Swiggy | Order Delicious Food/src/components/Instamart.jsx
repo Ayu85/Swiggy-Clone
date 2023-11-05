ad1039b7a658cfc587b036aa1ac6cfac9ce5900d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-05 10:49:02 +0000

    header part 10% done

--- Swiggy | Order Delicious Food/src/components/Instamart.jsx
+++ Swiggy | Order Delicious Food/src/components/Instamart.jsx
@@ -7,21 +7,24 @@ import skinLogo from '../assets/instamartAsset/skin.png'
 import wDress from '../assets/instamartAsset/w-dress.webp'
 import mDress from '../assets/instamartAsset/m-dress.avif'
 import perfLogo from '../assets/instamartAsset/perfumes.webp'
-
+import phoneLogo from '../assets/instamartAsset/phone.jpeg'
 
 
 function Instamart() {
     return (
         <div>
             <div className='header-logo'><img src={logo} /></div>
-            <div>
+            <div className='categories-wrapper'>
                 {/* categories part */}
                 <div><img src={laptopLogo} /></div>
-                <div><img  /></div>
-                <div><img src={laptopLogo} /></div>
-                <div><img src={laptopLogo} /></div>
-                <div><img src={laptopLogo} /></div>
-                <div><img src={laptopLogo} /></div>
+                <div><img src={phoneLogo} /></div>
+                <div><img src={mDress} /></div>
+                <div><img src={wDress} /></div>
+                <div><img src={skinLogo} /></div>
+                <div><img src={perfLogo} /></div>
+                <div><img src={grocLogo} /></div>
+                <div><img src={furntLogo} /></div>
+                <div><img src={decorlogo} /></div>
 
             </div>
         </div>
