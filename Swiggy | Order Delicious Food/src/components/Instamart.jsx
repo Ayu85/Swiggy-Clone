60477e7ea3b8ab6fe51efc66f0788fb5bbbd11dc
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-05 10:36:48 +0000

    categories part started

--- Swiggy | Order Delicious Food/src/components/Instamart.jsx
+++ Swiggy | Order Delicious Food/src/components/Instamart.jsx
@@ -1,8 +1,29 @@
-import logo from '../assets/header-logo.png'
+import logo from '../assets/instamartAsset/header-logo.png'
+import decorlogo from '../assets/instamartAsset/decor.jpeg'
+import furntLogo from '../assets/instamartAsset/furniture.jpg'
+import grocLogo from '../assets/instamartAsset/groceries.jpeg'
+import laptopLogo from '../assets/instamartAsset/laptop.jpeg'
+import skinLogo from '../assets/instamartAsset/skin.png'
+import wDress from '../assets/instamartAsset/w-dress.webp'
+import mDress from '../assets/instamartAsset/m-dress.avif'
+import perfLogo from '../assets/instamartAsset/perfumes.webp'
+
+
+
 function Instamart() {
     return (
         <div>
             <div className='header-logo'><img src={logo} /></div>
+            <div>
+                {/* categories part */}
+                <div><img src={laptopLogo} /></div>
+                <div><img  /></div>
+                <div><img src={laptopLogo} /></div>
+                <div><img src={laptopLogo} /></div>
+                <div><img src={laptopLogo} /></div>
+                <div><img src={laptopLogo} /></div>
+
+            </div>
         </div>
 
     )
