c9bb6adfb32b7e63ed2bdd68f6d379bb4d725f97
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-06 13:35:37 +0000

    about config

---
+++ Swiggy | Order Delicious Food/src/components/About.jsx
@@ -0,0 +1,8 @@
+function About(){
+    return(
+        <div className="about">
+            <img src="https://miro.medium.com/v2/resize:fit:1400/1*qvhMfaZBh8cHOuOJP2myTA.png"/>
+        </div>
+    )
+}
+export default About;
